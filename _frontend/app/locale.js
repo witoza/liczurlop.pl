d6852eb4eb23e3a2948443fad1f5efca34455276
witold.zegarowski <witold.zegarowski@gmail.com>
2016-09-03 15:42:02 +0200

    v2

--- _frontend/app/locale.js
+++ _frontend/app/locale.js
@@ -1,17 +1,31 @@
 ﻿const public_holidays = [
-    {date: new Date("1/1/2016").getTime(), name: "Nowy Rok, Swietej Bozej Rodzicielki"},
-    {date: new Date("1/6/2016").getTime(), name: "Trzech Króli (Objawienie Panskie)"},
+    {date: new Date("1/1/2016").getTime(), name: "Nowy Rok, Świetej Bożej Rodzicielki"},
+    {date: new Date("1/6/2016").getTime(), name: "Trzech Króli (Objawienie Pańskie)"},
     {date: new Date("3/27/2016").getTime(), name: "Wielkanoc"},
     {date: new Date("3/28/2016").getTime(), name: "Poniedzialek Wielkanocny"},
-    {date: new Date("5/1/2016").getTime(), name: "Swieto Pracy"},
-    {date: new Date("5/3/2016").getTime(), name: "Swieto Konstytucji 3 Maja"},
-    {date: new Date("5/15/2016").getTime(), name: "Zeslanie Ducha Swietego (Zielone Swiatki)"},
-    {date: new Date("5/26/2016").getTime(), name: "Boze Cialo"},
-    {date: new Date("8/15/2016").getTime(), name: "Swieto Wojska Polskiego, Wniebowziecie Najswietszej Maryi Panny"},
-    {date: new Date("11/1/2016").getTime(), name: "Wszystkich Swietych"},
-    {date: new Date("11/11/2016").getTime(), name: "Swieto Niepodleglosci"},
-    {date: new Date("12/25/2016").getTime(), name: "Boze Narodzenie (pierwszy dzien)"},
-    {date: new Date("12/26/2016").getTime(), name: "Boze Narodzenie (drugi dzien)"}
+    {date: new Date("5/1/2016").getTime(), name: "Święto Pracy"},
+    {date: new Date("5/3/2016").getTime(), name: "Święto Konstytucji 3 Maja"},
+    {date: new Date("5/15/2016").getTime(), name: "Zesłanie Ducha Swietego (Zielone Swiatki)"},
+    {date: new Date("5/26/2016").getTime(), name: "Boże Ciało"},
+    {date: new Date("8/15/2016").getTime(), name: "Święto Wojska Polskiego, Wniebowziecie Najswietszej Maryi Panny"},
+    {date: new Date("11/1/2016").getTime(), name: "Wszystkich Świetych"},
+    {date: new Date("11/11/2016").getTime(), name: "Święto Niepodleglosci"},
+    {date: new Date("12/25/2016").getTime(), name: "Boże Narodzenie (pierwszy dzień)"},
+    {date: new Date("12/26/2016").getTime(), name: "Boże Narodzenie (drugi dzień)"},
+
+    {date: new Date("1/1/2017").getTime(), name: "Nowy Rok, Świetej Bożej Rodzicielki"},
+    {date: new Date("1/6/2017").getTime(), name: "Trzech Króli (Objawienie Pańskie)"},
+    {date: new Date("4/16/2017").getTime(), name: "Wielkanoc"},
+    {date: new Date("4/17/2017").getTime(), name: "Poniedzialek Wielkanocny"},
+    {date: new Date("5/1/2017").getTime(), name: "Święto Pracy"},
+    {date: new Date("5/3/2017").getTime(), name: "Święto Konstytucji 3 Maja"},
+    {date: new Date("6/4/2017").getTime(), name: "Zesłanie Ducha Swietego (Zielone Swiatki)"},
+    {date: new Date("6/15/2017").getTime(), name: "Boże Ciało"},
+    {date: new Date("8/15/2017").getTime(), name: "Święto Wojska Polskiego, Wniebowziecie Najswietszej Maryi Panny"},
+    {date: new Date("11/1/2017").getTime(), name: "Wszystkich Świetych"},
+    {date: new Date("11/11/2017").getTime(), name: "Święto Niepodleglosci"},
+    {date: new Date("12/25/2017").getTime(), name: "Boże Narodzenie (pierwszy dzień)"},
+    {date: new Date("12/26/2017").getTime(), name: "Boże Narodzenie (drugi dzień)"}
 ];
 
 const TRANSLATE = {
